d434572024d7367ac8bf8bb775ef9bd23729e5ab
AntonTaran <taran.151290@gmail.com>
2023-09-06 13:58:09 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const ids = [1, 1, 3, 4, 5];
+let newIds = [...ids];
+
+const union = (p, q) => {
+  const pId = ids[p];
+  const qId = ids[q];
+
+  for (let index = 0; index <= ids.length; index++) {
+    // find all pId and change to qId
+    if (ids[index] === pId) newIds[index] = qId;
+  }
+};
+
+union(2, 4); // newIds = [1, 1, 5, 4, 5]
